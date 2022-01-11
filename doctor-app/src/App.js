1247a4c32dcecc466f8e1a552f6f10f1278a0325
Shahad-F <shahadbinsweleh@gmail.com>
2022-01-11 16:06:26 +0300

    Homepage

--- doctor-app/src/App.js
+++ doctor-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Sinup from './comp/doctor/Sinup';
-
+ import Navbar from './comp/Navbar'
 function App() {
   return (
     <div className="App">
-      <Sinup /> 
+      
+      
+      <Navbar/>
+      
     </div>
   );
 }
--- doctor-app/src/comp/Navbar.js
+++ doctor-app/src/comp/Navbar.js
@@ -5,7 +5,7 @@ import Home from './Home';
 function Navbar() {
     return ( <>
     
-    {/* <h2>navbar </h2> */}
+     
     <Home/>
 
     </> );
