53a48ae7335589cf4ab8cd39e9e901a6b5ec9b68
KhaledAlhamad <khaledalhamad997@gmail.com>
2022-01-11 15:32:46 +0300

    updated appointments route

--- backend/routes/appointment.js
+++ backend/routes/appointment.js
@@ -15,11 +15,34 @@ router.post("/", async (req, res) => {
   });
 });
 
+
 router.get("/", async (req, res) => {
   const id = req.params.id;
   Appointment.find({}, (err, Appointments) => {
     res.status(200).send(Appointments);
   });
 });
+
+router.put("/:id", async (req, res) => {
+    const newAppointment = req.body;
+    Appointment.updateOne({_id: req.params.id} ,newAppointment, (err, appointments) => {
+      if (appointments) {
+        res.status(200).send(appointments);
+      } else {
+        res.status(500).send(err);
+      }
+    });
+  });
+
+  router.delete("/:id", async (req, res) => {
+    const newAppointment = req.body;
+    Appointment.findByIdAndDelete( req.params.id , (err, appointments) => {
+      if (appointments) {
+        res.status(200).send(appointments);
+      } else {
+        res.status(500).send(err);
+      }
+    });
+  });
 
 module.exports = router;
