b1a4d55e16d1b658a481e6f2cddfb1a6a4a39acd
KhaledAlhamad <khaledalhamad997@gmail.com>
2022-01-11 14:09:48 +0300

    second commit

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,20 @@
 const express = require("express")
+const app = express();
+const PORT = 8000;
+const cors = require("cors");
+app.use(cors());
+const mongoose = require("mongoose");
+
+const Appointment = require("./routes/appointment")
+app.use("/appointment", Appointment);
+
+
+async function main() {
+    await mongoose.connect('mongodb+srv://user1:user2022@js.cj74d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+  }
+  main().catch((err) => console.log(err));
+
+app.listen(PORT , (err) =>{
+    if(err) console.log("ERROR" + err)
+    console.log("listening on PORT" + PORT)
+})
