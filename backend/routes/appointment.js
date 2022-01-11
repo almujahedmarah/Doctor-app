89fce429edf0f8e3d700d22e70fa6194fde883ee
KhaledAlhamad <khaledalhamad997@gmail.com>
2022-01-11 14:58:31 +0300

    updated appointment route

--- backend/routes/appointment.js
+++ backend/routes/appointment.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const Appointment = require("../models/appointment").Appointment;
-const router = require("express").Router()
+const router = require("express").Router();
 const mongoose = require("mongoose");
-
+router.use(express.json());
 
 router.post("/", async (req, res) => {
-    const newAppointment = new Appointment(req.body);
-  
-    try {
-      const savedAppointment = await newAppointment.save();
-      res.status(200).json(savedCart);
-    } catch (err) {
-      res.status(500).json(err);
+  const newAppointment = req.body;
+  Appointment.create(newAppointment, (err, appointments) => {
+    if (appointments) {
+      res.status(200).send(appointments);
+    } else {
+      res.status(500).send(err);
     }
   });
+});
 
+router.get("/", async (req, res) => {
+  const id = req.params.id;
+  Appointment.find({}, (err, Appointments) => {
+    res.status(200).send(Appointments);
+  });
+});
 
-  module.exports = router;
+module.exports = router;
