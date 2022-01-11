eb6ec202b79e0218eae120eaf08824eb6d2ef14d
Shahad-F <shahadbinsweleh@gmail.com>
2022-01-11 13:49:30 +0300

    1st push

---
+++ doctor-app/src/comp/Home.js
@@ -0,0 +1,31 @@
+
+
+import './CSS/home.css'
+
+function Home() {
+    return ( <>
+    
+ 
+
+<div className='parent'>
+
+<div className='partone'>
+    <h1 className='title'>Doctor App</h1>
+
+    <button>Get Start</button>
+</div>
+
+<div className='parttwo'>
+
+ 
+ </div>
+
+
+
+
+</div>
+
+    </> );
+}
+
+export default Home;
---
+++ doctor-app/src/comp/Navbar.js
@@ -0,0 +1,14 @@
+
+
+import Home from './Home';
+
+function Navbar() {
+    return ( <>
+    
+    {/* <h2>navbar </h2> */}
+    <Home/>
+
+    </> );
+}
+
+export default Navbar;
