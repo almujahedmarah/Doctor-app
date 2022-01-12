0341abe2cdcf98e590d7c9e16ef9a7832af8cf46
as-test <ashraf.alghamdi.ruh.js@tuwaiq.edu.sa>
2022-01-12 11:57:52 +0300

    Doctors model and route modified. Main route modified

--- backend/app.js
+++ backend/app.js
@@ -6,16 +6,27 @@ app.use(cors());
 const mongoose = require("mongoose");
 
 const Appointment = require("./routes/appointment")
-app.use("/appointment", Appointment);
-
-
+const Doctors = require("./routes/doctors")
 
 async function main() {
-    await mongoose.connect('mongodb+srv://user1:user2022@js.cj74d.mongodb.net/doctorApp?retryWrites=true&w=majority');
-  }
-  main().catch((err) => console.log(err));
+  await mongoose.connect('mongodb+srv://user1:user2022@js.cj74d.mongodb.net/doctorApp?retryWrites=true&w=majority');
+}
+main().catch((err) => console.log(err));
+
+app.use("/", Appointment)
+app.use("/", Doctors)
+app.use("/appointment", Appointment);
+app.use("/doctorGet", Doctors)
+app.use("/doctorAdd", Doctors)
+app.use("/doctorUpdate", Doctors)
+app.use("/doctorDelete", Doctors)
+
+
 
 app.listen(PORT , (err) =>{
     if(err) console.log("ERROR" + err)
-    console.log("i work on PORT" + PORT)
+    console.log("listening on PORT" + PORT)
 })
+
+
+module.exports = app;
--- backend/routes/doctors.js
+++ backend/routes/doctors.js
@@ -6,18 +6,46 @@ const Doctors = require("../models/doctors.js");
 router.use(express.urlencoded({extended: true}));
 router.use(express.json());
 
-async function main(){
-    await mongoose.connect("mongodb+srv://user1:user2022@js.cj74d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-}
 
-main().catch(err => console.log(err));
+router.get('/', (req, res) => {
+    res.send("You cannot access this page")
+})
 
-router.get("/getDoctors", (req, res) => {
+router.get("/doctorGet", (req, res) => {
     Doctors.find({}, (err, data) => {
         if(err) console.log(err);
-        res.json(data);
+        res.send(data);
         mongoose.connection.close;
     })
+})
+
+router.get("/doctorAdd", (req, res) => {
+    Doctors.create({
+        _id: req.body.id,
+        name: req.body.name,
+        speciality: req.body.speciality,
+    }, (err, res) => {
+        if(err) console.log(err);
+        mongoose.connection.close;
+    })
+})
+
+router.get("/doctorUpdate", (req, res) => {
+    Doctors.updateOne({_id: req.body.id}, 
+        {
+            name: req.body.name,
+            speciality: req.body.speciality
+        }, (err, res) => {
+            if(err) console.log(err);
+            mongoose.connection.close;
+        })
+})
+
+router.get("/doctorDelete", (req, res) => {
+    Doctors.deleteOne({_id: req.body.id}, (err, res) => {
+        if(err) console.log(err);
+        mongoose.connection.close;
+        })
 })
 
 
