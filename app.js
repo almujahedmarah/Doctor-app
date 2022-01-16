4bedd11cc2a334a5624def14b02af71119312e8b
almujahedmarah <marah.almujahed.ruh.js@tuwaiq.edu.sa>
2022-01-16 11:31:05 +0300

    done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express();
-const PORT = 8000 || process.env.PORT;
+const PORT =  process.env.PORT || 8000;
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
